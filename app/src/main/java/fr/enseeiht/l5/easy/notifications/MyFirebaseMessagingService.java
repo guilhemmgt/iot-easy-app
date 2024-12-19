41b391ee3fee894607718e0db0826d27ea91f7c7
SANGOI Remy <rsangoi@minotaure.enseeiht.fr>
2024-12-19 14:17:57 +0100

    First commit

---
+++ app/src/main/java/fr/enseeiht/l5/easy/notifications/MyFirebaseMessagingService.java
@@ -0,0 +1,21 @@
+package fr.enseeiht.l5.easy.notifications;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import androidx.core.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+}
